17ee8f69bb4274c369c333a6a25c61f9ea4039bf
Maciej Czerwonka <czerwonkamaciej25@gmail.com>
2023-01-17 15:50:50 +0100

    V3

---
+++ swiper.js
@@ -0,0 +1,10 @@
+// import Swiper from "https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js";
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
