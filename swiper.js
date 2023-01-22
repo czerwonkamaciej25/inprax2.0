04f96950cc2708b177709162b8dbe3df55546973
Maciej Czerwonka <czerwonkamaciej25@gmail.com>
2023-01-22 01:20:30 +0100

    swiper

--- swiper.js
+++ swiper.js
@@ -1,10 +1,31 @@
 // import Swiper from "https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js";
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  
+  
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  mousewheel: true,
+  keyboard: true,
+
+  effect: "coverflow",
+  spaceBetween: 100,
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 3,
+      coverflowEffect: {
+        rotate: 2,
+        stretch: 0,
+        depth: 70,
+        modifier: 1,
+        slideShadows: false,
+      },
+});
