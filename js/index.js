75d08f32bef2a9b0b9069710bf36d813b4d2e001
Maciej Czerwonka <czerwonkamaciej25@gmail.com>
2023-06-27 13:53:39 +0200

    poprawki2

--- js/index.js
+++ js/index.js
@@ -1,35 +1,88 @@
 function submitForm(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    console.log("submit");
+  event.preventDefault();
+  event.stopPropagation();
+  console.log("submit");
 
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-    var phone = document.getElementById("phone").value;
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var phone = document.getElementById("phone").value;
 
-    $.ajax({
-      type: "POST",
-      url: "/php/contact-form.php",
-      data: {
-        name: name,
-        email: email,
-        message: message,
-        phone: phone,
-      },
-      success: function (data) {
-        const response = JSON.parse(data);
-        document.getElementById("form-result").innerHTML = response.message;
-        if (response.status === "success") {
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          document.getElementById("phone").value = "";
-        }
-      },
-      error: function (data) {
-        document.getElementById("form-result").innerHTML = data;
-      },
-    });
+  // Resetowanie klas error-input dla wszystkich pól
+  document.getElementById("name").classList.remove("error-input");
+  document.getElementById("email").classList.remove("error-input");
+  document.getElementById("message").classList.remove("error-input");
+  document.getElementById("phone").classList.remove("error-input");
+
+  var errors = []; // Tablica przechowująca nieuzupełnione pola
+
+  // Sprawdzanie, czy pole name jest puste
+  if (name === "") {
+    errors.push("Pole Imię jest wymagane");
+    document.getElementById("name").classList.add("error-input");
   }
 
+  // Sprawdzanie, czy pole email jest puste lub nieprawidłowe
+  if (email === "" || !validateEmail(email)) {
+    errors.push("Podaj prawidłowy adres email");
+    document.getElementById("email").classList.add("error-input");
+  }
+
+  // Sprawdzanie, czy pole message jest puste
+  if (message === "") {
+    errors.push("Pole Wiadomość jest wymagane");
+    document.getElementById("message").classList.add("error-input");
+  }
+
+  // Sprawdzanie, czy pole phone jest puste
+  if (phone === "") {
+    errors.push("Pole Telefon jest wymagane");
+    document.getElementById("phone").classList.add("error-input");
+  }
+
+  if (errors.length > 0) {
+    // Tworzenie wiadomości z informacją o nieuzupełnionych polach
+    var errorMessages = "<ul>";
+    errors.forEach(function (error) {
+      errorMessages += "<li>" + error + "</li>";
+    });
+    errorMessages += "</ul>";
+
+    document.getElementById("form-result").innerHTML =
+      '<div class="alert alert-danger mt-2">' +
+      errorMessages +
+      "</div>";
+
+    return; // Zatrzymywanie dalszego wykonywania funkcji
+  }
+
+  $.ajax({
+    type: "POST",
+    url: "/php/contact-form.php",
+    data: {
+      name: name,
+      email: email,
+      message: message,
+      phone: phone,
+    },
+    success: function (data) {
+      const response = JSON.parse(data);
+      document.getElementById("form-result").innerHTML = response.message;
+      if (response.status === "success") {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        document.getElementById("phone").value = "";
+      }
+    },
+    error: function (data) {
+      document.getElementById("form-result").innerHTML = data;
+    },
+  });
+}
+
+// Funkcja sprawdzająca poprawność adresu email
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
--- php/contact-form.php
+++ php/contact-form.php
@@ -4,48 +4,59 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // process your $_POST here
   $name = $_POST['name'];                 // Sender's name
   $email = $_POST['email'];     // Sender's email address
-  $phone  = $_POST['phone'];     // Sender's email address
+  $phone  = $_POST['phone'];     // Sender's phone number
   $message = $_POST['message'];    // Sender's message
   $from = 'Inprax - formularz kontaktowy ';
-  $to = 'czerwonkamaciej25@gmail.com';     // Recipient's email address
+  $to = 'maciej.czerwonka@inprax.pl';     // Recipient's email address
   $subject = 'Formularz kontaktowy INPRAX';
 
-  $body ="From: $name \n E-Mail: $email \n Phone : $phone \n Message : $message"  ;
+  $body = "From: $name \n E-Mail: $email \n Phone : $phone \n Message : $message";
 
-	// init error message
-	$errmsg = '';
+  // init error message
+  $errmsg = [];
   // Check if name has been entered
-  if (!$_POST['name']) {
-   $errmsg = 'Pole wymagane';
+  if (empty($name)) {
+    $errmsg[] = 'Pole Imię jest wymagane';
   }
 
   // Check if email has been entered and is valid
-  if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-   $errmsg = 'Nieprawidłowy format adresu e-mail';
+  if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errmsg[] = 'Podaj prawidłowy adres email';
   }
 
-  //Check if message has been entered
-  if (!$_POST['message']) {
-   $errmsg = 'Pole wymagane';
+  // Check if message has been entered
+  if (empty($message)) {
+    $errmsg[] = 'Pole Wiadomość jest wymagane';
   }
 
-	$result = '';
+  // Check if phone has been entered
+  if (empty($phone)) {
+    $errmsg[] = 'Pole Telefon jest wymagane';
+  }
+
+  $result = '';
   $status = 'success';
   // If there are no errors, send the email
-  if (!$errmsg) {
-		if (mail ($to, $subject, $body, $from)) {
-			$result='<div class="alert alert-success mt-2">Wiadomość wysłana</div>';
+  if (empty($errmsg)) {
+    if (mail($to, $subject, $body, $from)) {
+      $result =
+        '<div class="alert alert-success mt-2">Wiadomość wysłana</div>';
       $status = 'success';
-		}
-		else {
-		  $result='<div class="alert alert-danger mt-2">Sorry there was an error sending your message. Please try again later.</div>';
+    } else {
+      $result =
+        '<div class="alert alert-danger mt-2">Sorry there was an error sending your message. Please try again later.</div>';
       $status = 'error';
-		}
-	}
-	else{
-		$result='<div class="alert alert-danger mt-2">'.$errmsg.'</div>';
+    }
+  } else {
+    $errorMessages = '<ul>';
+    foreach ($errmsg as $error) {
+      $errorMessages .= '<li>' . $error . '</li>';
+    }
+    $errorMessages .= '</ul>';
+
+    $result = '<div class="alert alert-danger mt-2">' . $errorMessages . '</div>';
     $status = 'error';
-	}
+  }
 
   echo json_encode(["message" => $result, "status" => $status]);
 }
